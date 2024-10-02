d7ba974e5b021eb596f63274c0235ee98076b3f3
Davit <davedabyeet@gmail.com>
2024-10-03 01:00:01 +0400

    active link matcher

--- src/components/Header/helpers/getCurrentPathIndex.ts
+++ src/components/Header/helpers/getCurrentPathIndex.ts
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 export const getCurrentPathIndex = (navLinks: ReactNode[], pathname: string): number => {
 	return navLinks.findIndex((link) => {
 		if (React.isValidElement(link) && link.props.href) {
-			return link.props.href === pathname;
+			return pathname.startsWith(link.props.href)
 		}
 		return false;
 	});
