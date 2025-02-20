b45a39f969af94b0c30b809d561e433770506dd0
Davit <davedabyeet@gmail.com>
2025-02-20 09:21:50 +0400

    inject data-cy into <TreeSelect/> treeData

--- src/components/TaskTreeSelector/TaskTreeSelector.tsx
+++ src/components/TaskTreeSelector/TaskTreeSelector.tsx
@@ -8,16 +8,27 @@ type UITreeSelectProps = { sectionsCheckable?: boolean } & Omit<
 	'multiple' | 'treeIcon' | 'treeDefaultExpandAll' | 'treeLine' | 'treeCheckable'
 >;
 
+const addDataCyToTreeData = (data: any[], dataCy: string): any[] => {
+	return data.map((item) => ({
+		...item,
+		title: <span data-cy={dataCy}>{item.title}</span>,
+		children: item.children ? addDataCyToTreeData(item.children, dataCy) : [],
+	}));
+};
+
 const InternalTreeSelector: React.ForwardRefRenderFunction<any, UITreeSelectProps> = (
 	{ treeData, placeholder, sectionsCheckable = false, ...rest },
 	ref,
 ) => {
+	// @ts-ignore
+	const treeDataWithTestId = addDataCyToTreeData(treeData, 'tree-item');
+
 	return (
 		<TreeSelect
 			ref={ref}
 			placeholder={placeholder || 'Select an action'}
 			treeDefaultExpandAll={true}
-			treeData={treeData}
+			treeData={treeDataWithTestId}
 			treeLine={false}
 			multiple={false}
 			treeIcon={true}
@@ -25,9 +36,6 @@ const InternalTreeSelector: React.ForwardRefRenderFunction<any, UITreeSelectProp
 			popupClassName="ui-tree"
 			dropdownRender={(menu) => <StyledDropdown>{menu}</StyledDropdown>}
 			dropdownStyle={{ width: 'max-content', minWidth: '550px' }}
-			treeTitleRender={(node) => {
-				return <span data-cy="tree-item">{node.title}</span>;
-			}}
 			{...rest}
 		/>
 	);
