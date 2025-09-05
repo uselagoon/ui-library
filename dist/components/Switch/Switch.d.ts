576eb1ca09a0eaabf84e6a906058cb5d9671b86c
cgoodwin90 <chris_2190@hotmail.com>
2025-09-05 20:42:15 +1000

    Updates Switch to handle loading state

--- dist/components/Switch/Switch.d.ts
+++ dist/components/Switch/Switch.d.ts
@@ -4,7 +4,8 @@ type SwitchProps = React.ComponentProps<typeof Switch> & {
     label: string;
     description: string;
     id: string;
+    loading?: boolean;
 };
-export default function SwitchWithDescription({ id, label, description, ...rest }: SwitchProps): import("react/jsx-dev-runtime").JSX.Element;
+export default function SwitchWithDescription({ id, label, description, disabled, loading, ...rest }: SwitchProps): import("react/jsx-dev-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Switch.d.ts.map
--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Label } from '../ui/label';
 import { Switch } from '../ui/switch';
+import { Loader2 } from 'lucide-react';
 
 type SwitchProps = React.ComponentProps<typeof Switch> & {
 	label: string;
 	description: string;
 	id: string;
+	loading?: boolean;
 };
 
-export default function SwitchWithDescription({ id, label, description, ...rest }: SwitchProps) {
+export default function SwitchWithDescription({ id, label, description, disabled, loading, ...rest }: SwitchProps) {
 	return (
 		<div className="flex space-x-2">
-			<Switch id={id} {...rest} />
+				<Switch id={id} disabled={disabled || loading} {...rest} />
 			<div className="space-y-2">
-				<Label className="pt-[3px]" htmlFor={id}>
-					{label}
+				<Label className={`pt-[3px] ${loading ? 'opacity-60' : ''}`} htmlFor={id}>
+					{loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : label}
 				</Label>
-				<span className="text-[#737373]">{description}</span>
+				<span className={`text-[#737373] ${loading ? 'opacity-60' : ''}`}>{description}</span>
 			</div>
 		</div>
 	);
