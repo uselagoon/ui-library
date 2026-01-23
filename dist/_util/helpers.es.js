9723c6c4ebac13280d20b428b77e1abab9d26b5f
cgoodwin90 <chris_2190@hotmail.com>
2026-01-23 16:07:58 +1100

    Adds build files

--- dist/_util/helpers.es.js
+++ dist/_util/helpers.es.js
@@ -1,18 +1,33 @@
 import "react/jsx-runtime";
 import "react";
 import "react-highlight-words";
-const F = (n, e) => {
-  const r = (a) => a.charCodeAt(0) - 64, t = (a) => Math.round(r(a) * 11);
-  let o = t(n) % 256, u = t(e) % 256, l = Math.round((r(n) + r(e)) / 2 * 11) % 256;
+const p = (t, e) => {
+  const o = (a) => a.charCodeAt(0) - 64, r = (a) => Math.round(o(a) * 11);
+  let n = r(t) % 256, l = r(e) % 256, c = Math.round((o(t) + o(e)) / 2 * 11) % 256;
   return {
-    bgColor: `rgb(${o}, ${u}, ${l})`,
-    textColor: c(o, u, l) > 0.5 ? "#000000" : "#FFFFFF"
+    bgColor: `rgb(${n}, ${l}, ${c})`,
+    textColor: s(n, l, c) > 0.5 ? "#000000" : "#FFFFFF"
   };
 };
-function c(n, e, r) {
-  const t = [n, e, r].map((o) => (o /= 255, o <= 0.03928 ? o / 12.92 : Math.pow((o + 0.055) / 1.055, 2.4)));
-  return 0.2126 * t[0] + 0.7152 * t[1] + 0.0722 * t[2];
+function s(t, e, o) {
+  const r = [t, e, o].map((n) => (n /= 255, n <= 0.03928 ? n / 12.92 : Math.pow((n + 0.055) / 1.055, 2.4)));
+  return 0.2126 * r[0] + 0.7152 * r[1] + 0.0722 * r[2];
 }
+const g = (t) => t * 24 * 60 * 60 * 1e3, d = (t, e, o) => {
+  const n = {
+    value: e,
+    expiry: (/* @__PURE__ */ new Date()).getTime() + g(o)
+  };
+  localStorage.setItem(t, JSON.stringify(n));
+}, S = (t) => {
+  const e = localStorage.getItem(t);
+  if (!e)
+    return null;
+  const o = JSON.parse(e);
+  return o.expiry < Date.now() ? (localStorage.removeItem(t), null) : o.value;
+};
 export {
-  F as genAvatarBackground
+  p as genAvatarBackground,
+  S as getLocalStorage,
+  d as setLocalStorage
 };
