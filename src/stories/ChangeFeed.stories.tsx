e91fb1bd1d20ba08dd3bc59dbfc73d45346da547
cgoodwin90 <chris_2190@hotmail.com>
2026-01-29 15:25:12 +1100

    Build files & minor Story update

--- src/stories/ChangeFeed.stories.tsx
+++ src/stories/ChangeFeed.stories.tsx
@@ -12,30 +12,35 @@ type Story = StoryObj<typeof ChangeFeedContainer>;
 
 export const Default: Story = {
     args: {
+        sourceData: 'http://invalid-url-for-test.com/feed.json',
         fallbackData: sampleActivityData,
     },
 };
 
 export const Empty: Story = {
     args: {
+        sourceData: 'http://invalid-url-for-test.com/feed.json',
         fallbackData: [],
     },
 };
 
 export const SingleItem: Story = {
     args: {
+        sourceData: 'http://invalid-url-for-test.com/feed.json',
         fallbackData: [sampleActivityData[0]],
     },
 };
 
 export const NoTags: Story = {
     args: {
+        sourceData: 'http://invalid-url-for-test.com/feed.json',
         fallbackData: sampleActivityData.map(item => ({ ...item, tags: undefined })),
     },
 };
 
 export const NoDescription: Story = {
     args: {
+        sourceData: 'http://invalid-url-for-test.com/feed.json',
         fallbackData: sampleActivityData.map(item => ({ ...item, description: '' })),
     },
 };
